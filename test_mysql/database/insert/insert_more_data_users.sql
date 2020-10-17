1e3ebed2cd13fb959bac7d43371c4bfe49ea4208
Florent Poinsard <florent.poinsard@outlook.fr>
2020-10-17 22:07:16 +0200

    Improvement of the Makefile with working commands to locally run sharded kubernetes vitess cluster

---
+++ test_mysql/database/insert/insert_more_data_users.sql
@@ -0,0 +1,18 @@
+USE users;
+INSERT INTO user (timestamp,id,uid,name,gender,email,phone,dept,grade,language,region,role,preferTags,obtainedCredits) VALUES
+  ("1506328859005", "u5", "5", "user5", "male", "email5", "phone5", "dept16", "grade4", "zh", "Beijing", "role2", "tags11", "16"),
+  ("1506328859006", "u6", "6", "user6", "female", "email6", "phone6", "dept9", "grade2", "en", "Beijing", "role0", "tags4", "49"),
+  ("1506328859007", "u7", "7", "user7", "male", "email7", "phone7", "dept6", "grade4", "zh", "Beijing", "role1", "tags23", "8"),
+  ("1506328859008", "u8", "8", "user8", "male", "email8", "phone8", "dept5", "grade1", "zh", "Beijing", "role0", "tags3", "39"),
+  ("1506328859009", "u9", "9", "user9", "female", "email9", "phone9", "dept9", "grade4", "zh", "Beijing", "role0", "tags9", "69"),
+  ("1506328859010", "u10", "10", "user10", "female", "email10", "phone10", "dept17", "grade3", "zh", "Beijing", "role2", "tags48", "2"),
+  ("1506328859011", "u11", "11", "user11", "female", "email11", "phone11", "dept14", "grade1", "en", "Hong Kong", "role0", "tags8", "56"),
+  ("1506328859012", "u12", "12", "user12", "female", "email12", "phone12", "dept12", "grade2", "zh", "Hong Kong", "role0", "tags18", "3"),
+  ("1506328859013", "u13", "13", "user13", "female", "email13", "phone13", "dept3", "grade4", "zh", "Hong Kong", "role0", "tags10", "63"),
+  ("1506328859014", "u14", "14", "user14", "male", "email14", "phone14", "dept18", "grade1", "zh", "Hong Kong", "role2", "tags43", "10"),
+  ("1506328859015", "u15", "15", "user15", "female", "email15", "phone15", "dept3", "grade2", "zh", "Beijing", "role0", "tags46", "60"),
+  ("1506328859016", "u16", "16", "user16", "male", "email16", "phone16", "dept19", "grade3", "zh", "Hong Kong", "role1", "tags21", "55"),
+  ("1506328859017", "u17", "17", "user17", "male", "email17", "phone17", "dept7", "grade4", "en", "Beijing", "role1", "tags20", "51"),
+  ("1506328859018", "u18", "18", "user18", "female", "email18", "phone18", "dept12", "grade4", "zh", "Beijing", "role1", "tags1", "73"),
+  ("1506328859019", "u19", "19", "user19", "male", "email19", "phone19", "dept11", "grade1", "zh", "Beijing", "role1", "tags21", "69"),
+  ("1506328859020", "u20", "20", "user20", "male", "email20", "phone20", "dept4", "grade1", "zh", "Hong Kong", "role2", "tags18", "8");
---
+++ test_mysql/database/select/select_user_shard_2.sql
@@ -0,0 +1,3 @@
+\! echo 'Users 80-'
+USE users/80-;
+SELECT * FROM user LIMIT 20;

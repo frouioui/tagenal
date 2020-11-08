65f4dd3ad2014776bc80429b3ed73ea689887ed9
Florent Poinsard <florent.poinsard@outlook.fr>
2020-11-08 19:04:20 +0100

    TCP and HTTP traefik ingress routes for MySQL and MySQLMetrics

--- mysql/kubernetes/traefik/vitess/build.sh
+++ mysql/kubernetes/traefik/vitess/build.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
+cd "${0%/*}"
+
 yq w -i vitess_vtctld_ingress_route.yaml "spec.routes[0].services[0].name" $(kubectl get service --selector="planetscale.com/component=vtctld" -o custom-columns=":metadata.name" | head -n2)
+yq w -i vitess_vtctld_client_ingress_route.yaml "spec.routes[0].services[0].name" $(kubectl get service --selector="planetscale.com/component=vtctld" -o custom-columns=":metadata.name" | head -n2)
+yq w -i vitess_vtgate_ingress_route.yaml "spec.routes[0].services[0].name" $(kubectl get service --selector="planetscale.com/component=vtgate" -o custom-columns=":metadata.name" | head -n2)
+yq w -i vitess_vttablet_ingress_route.yaml "spec.routes[0].services[0].name" $(kubectl get service --selector="planetscale.com/component=vttablet" -o custom-columns=":metadata.name" | head -n2)

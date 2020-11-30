f7fd7028fb0b7e897f3a26abb18e0a55348fd613
Florent Poinsard <florent.poinsard@outlook.fr>
2020-11-30 20:35:13 +0100

    Addition of jaeger crd, operator, and ingress route + allinone integration

---
+++ scripts/jaeger.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+kubectl create namespace observability
+kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/crds/jaegertracing.io_jaegers_crd.yaml
+kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/service_account.yaml
+kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/role.yaml
+kubectl create -n observability -f https://raw.githubusercontent.com/jaegertracing/jaeger-operator/master/deploy/role_binding.yaml

b92076b03f6bef8de6d586a808c75852b0e68fc0
Florent Poinsard <florent.poinsard@outlook.fr>
2021-01-02 22:10:37 +0100

    Simpler data gen classes and new python command in readme

--- scripts/gen/README.md
+++ scripts/gen/README.md
@@ -5,7 +5,7 @@
 Execute the following command:
 
 ```
-python3 generation_data.py
+python3 generation_data.py --number-users 5000 --number-reads 20000 --number-articles 1000
 ```
 
 The data will be generated in the `article.sql`, `user.sql`, `user_read.sql`. Media and text files related to articles, will be store in the `articles` folder, where each subfolder corresponds to an article.
--- scripts/gen/generation_data.py
+++ scripts/gen/generation_data.py
@@ -1,5 +1,5 @@
 import json
-from random import random, randrange
+from random import random, randrange, choice
 import argparse
 import numpy as np
 from PIL import Image
@@ -52,49 +52,58 @@ class Generator:
         self.videos_path = args.videos_path
 
         self.uid_region = {}
+        self.uid_timestamp = {}
         self.aid_lang = {}
+        self.aid_timestamp = {}
         self.p = {}
-        self.p["Beijing" + "en"] = [
-            self.reads_beijing_en_ratio_agree,
-            self.reads_beijing_en_ratio_comment,
-            self.reads_beijing_en_ratio_read,
-            self.reads_beijing_en_ratio_share
-        ]
-        self.p["Beijing" + "zh"] = [
-            self.reads_beijing_zh_ratio_agree,
-            self.reads_beijing_zh_ratio_comment,
-            self.reads_beijing_zh_ratio_read,
-            self.reads_beijing_zh_ratio_share
-        ]
-        self.p["Hong Kong" + "en"] = [
-            self.reads_hong_kong_en_ratio_agree,
-            self.reads_hong_kong_en_ratio_comment,
-            self.reads_hong_kong_en_ratio_read,
-            self.reads_hong_kong_en_ratio_share 
-        ]
-        self.p["Hong Kong"+"zh"] = [
-            self.reads_hong_kong_zh_ratio_agree,
-            self.reads_hong_kong_zh_ratio_comment,
-            self.reads_hong_kong_zh_ratio_read,
-            self.reads_hong_kong_zh_ratio_share
-        ]
+        self.p["Beijing" + "en"] = [self.reads_beijing_en_ratio_agree, self.reads_beijing_en_ratio_comment, self.reads_beijing_en_ratio_read, self.reads_beijing_en_ratio_share]
+        self.p["Beijing" + "zh"] = [self.reads_beijing_zh_ratio_agree, self.reads_beijing_zh_ratio_comment, self.reads_beijing_zh_ratio_read, self.reads_beijing_zh_ratio_share]
+        self.p["Hong Kong" + "en"] = [self.reads_hong_kong_en_ratio_agree, self.reads_hong_kong_en_ratio_comment, self.reads_hong_kong_en_ratio_read, self.reads_hong_kong_en_ratio_share]
+        self.p["Hong Kong"+"zh"] = [self.reads_hong_kong_zh_ratio_agree, self.reads_hong_kong_zh_ratio_comment, self.reads_hong_kong_zh_ratio_read, self.reads_hong_kong_zh_ratio_share]
 
-    pass
+    def get_article_path(self, id, create=True):
+        path = os.path.join(self.output_path, 'articles/article' + str(id))
+        if not os.path.exists(path) and create == True:
+            os.makedirs(path)
+        assert os.path.isdir(path)
+        return path
 
-class GeneratorSQL(Generator):
-    def __init__(self, args):
-        super().__init__(args=args)
-        pass
+    def create_article_text_file(self, id, article_path):
+        text_file_name = "text_a" + str(id) + '.txt'
+        num_words = int(random() * self.articles_words_max)
+        text = ['tsinghua '] * num_words
+        f = open(article_path + "/" + text_file_name, 'w+', encoding="utf8")
+        f.write("".join(text))
+        f.close()
+        return text_file_name
 
-    def gen_an_user(self, i):
-        timeBegin = 1605624528
+    def create_article_images(self, id, article_path):
+        images_str = ""
+        image_num = int(random() * self.articles_images_max) + 1
+        images_list = [x for x in os.listdir(self.images_path) if os.path.isfile(os.path.join(self.images_path, x))]
+        for image_id in range(image_num):
+            image_str = 'image_a' + str(id) + '_' + str(image_id) + '.jpg'
+            images_str += image_str + ','
+            shutil.copyfile(self.images_path + '/' + choice(images_list), article_path + '/' + image_str)
+        return images_str
+
+    def create_article_video(self, id, article_path):
+        video_path = ""
+        if random() <= (self.articles_videos_percentage / 100):
+            videos_list = [x for x in os.listdir(self.videos_path) if os.path.isfile(os.path.join(self.videos_path, x))]
+            video_path = "video_a" + str(id) + '_video.flv'
+            shutil.copyfile(self.videos_path + '/' + choice(videos_list), article_path + '/' + video_path)
+        return video_path
+
+    def create_user(self, id):
+        timeBegin = 1410000000
         user = {}
-        user["timestamp"] = str(timeBegin + i)
-        user["uid"] = str(i)
-        user["name"] = "user%d" % i
+        user["timestamp"] = int(timeBegin + random() * 10000 + 10000 * id)
+        user["uid"] = str(id)
+        user["name"] = "user%d" % id
         user["gender"] = "male" if random() > 0.33 else "female"
-        user["email"] = "email%d" % i
-        user["phone"] = "phone%d" % i
+        user["email"] = "email%d" % id
+        user["phone"] = "phone%d" % id
         user["dept"]  = "dept%d" % int(random() * self.users_nb_depts)
         user["grade"] = "grade%d" % int(random() * 4 + 1)
         user["language"] = "en" if random() > 0.8 else "zh"
@@ -103,8 +112,64 @@ class GeneratorSQL(Generator):
         user["prefer_tags"] = "tags%d" % int(random() * self.users_nb_tags)
         user["obtained_credits"] = str(int(random() * self.users_max_credits))
         self.uid_region[user["uid"]] = user["region"]
+        self.uid_timestamp[user["uid"]] = user["timestamp"]
+        return user
+
+    def create_article(self, id):
+        timeBegin = 1415000000
+        article = {}
+        article["timestamp"] = int(timeBegin + random() * 10000 + 10000 * id)
+        article["aid"] = str(id)
+        article["title"] = "title%d" % id
+        article["category"] = "science" if random() < (self.articles_ratio_science/100) else "technology"
+        article["abstract"] = "abstract of article %d" % id
+        article["article_tags"] = "tags%d" % int(random() * self.articles_nb_tags)
+        article["authors"]  = "author%d" % int(random() * self.articles_nb_authors)
+        article["language"] = "en" if random() < (self.articles_ratio_english/100) else "zh"
+        self.aid_lang[article["aid"]] = article["language"]
+        self.aid_timestamp[article["aid"]] = article["timestamp"]
+        path = self.get_article_path(id)
+        article["text"] = self.create_article_text_file(id, path)
+        article["image"] = self.create_article_images(id, path)
+        article["video"] = self.create_article_video(id, path)
+        return article
+
+    def create_read(self, id):
+        timeBegin = 1430000000
+        read = {}
+        read["timestamp"] = int(timeBegin + random()*2000+2000*id)
+        read["uid"] = str(int(random() * (self.number_users-1)) + self.users_start_id)
+        read["aid"] = str(int(random() * (self.number_articles-1)) + self.articles_start_id)
+        while max(self.uid_timestamp[read["uid"]], self.aid_timestamp[read["aid"]]) > read["timestamp"]:
+            read["uid"] = str(int(random() * (self.number_users-1)) + self.users_start_id)
+            read["aid"] = str(int(random() * (self.number_articles-1)) + self.articles_start_id)
+        
+        region = self.uid_region[read["uid"]]
+        lang = self.aid_lang[read["aid"]]
+        ps = self.p[region + lang]
+
+        if (random() > ps[0]):
+            return self.create_read(id)
+        else:
+            read["read_or_not"] = "1"
+            read["read_time_length"] = str(int(random() * 100))
+            read["read_sequence"] = str(int(random() * 4))
+            read["agree_or_not"] = "1" if random() < ps[1] else "0"
+            read["comment_or_not"] = "1" if random() < ps[2] else "0"
+            read["share_or_not"] = "1" if random() < ps[3] else "0"
+            read["comment_detail"] = "comments to this article: (" + read["uid"] + "," + read["aid"] + ")"
+        return read
+    pass
+
+class GeneratorSQL(Generator):
+    def __init__(self, args):
+        super().__init__(args=args)
+        pass
+
+    def gen_an_user(self, i):
+        user = self.create_user(i)
         return "(" +  \
-                user["timestamp"] + ", " + \
+                str(user["timestamp"]) + ", " + \
                 "\"" + user["uid"] + "\", " + \
                 "\"" + user["name"] + "\", " + \
                 "\"" + user["gender"] + "\", " + \
@@ -120,45 +185,9 @@ class GeneratorSQL(Generator):
 
 
     def gen_an_article(self, i):
-        print(i)
-        timeBegin = 1605883728
-        article = {}
-        article["timestamp"] = str(timeBegin + i)
-        article["aid"] = str(i)
-        article["title"] = "title%d" % i
-        article["category"] = "science" if random() < (self.articles_ratio_science/100) else "technology"
-        article["abstract"] = "abstract of article %d" % i
-        article["article_tags"] = "tags%d" % int(random() * self.articles_nb_tags)
-        article["authors"]  = "author%d" % int(random() * self.articles_nb_authors)
-        article["language"] = "en" if random() > (self.articles_ratio_english/100) else "zh"
-        # article path
-        path = './articles/article'+str(i)
-        if not os.path.exists(path):
-            os.makedirs(path) 
-        # create text
-        article["text"] = "text_a"+str(i)+'.txt'
-        num = int(random()*self.articles_words_max)
-        text = ['tsinghua ']*num
-        f = open(path+"/text_a"+str(i)+'.txt','w+',encoding="utf8")
-        f.write("".join(text))
-        f.close()
-        # create images
-        image_num = int(random()*self.articles_images_max)+1
-        image_str = ""
-        for j in range(image_num):
-            image_str+= 'image_a'+str(i)+'_'+str(j)+'.jpg,'
-            shutil.copyfile(self.images_path+'/' + str(randrange(1,12)) + '.jpg', path + '/image_a' + str(i) + '_' + str(j) + '.jpg')
-        article["image"] = image_str
-        # create video
-        if random() < (self.articles_videos_percentage/100):
-            article["video"] = "video_a" + str(i) + '_video.flv'
-            shutil.copyfile(self.videos_path+'/video'+ str(randrange(1,2)) + '.flv', path + '/video_a' + str(i) +'_video.flv')
-        else:
-            article["video"] = ""
-
-        self.aid_lang[article["aid"]] = article["language"]
+        article = self.create_article(i)
         return "(" +  \
-                " " + article["timestamp"] + " , " + \
+                " " + str(article["timestamp"]) + " , " + \
                 "\"" + article["aid"] + "\", " + \
                 "\"" + article["title"] + "\", " + \
                 "\"" + article["category"] + "\", " + \
@@ -171,28 +200,9 @@ class GeneratorSQL(Generator):
                 "\"" + article["video"] + "\")";
 
     def gen_an_read(self, i):
-        timeBegin = 1606834128
-        read = {}
-        read["timestamp"] = str(timeBegin + i*10000)
-        read["uid"] = str(int(random() * (self.number_users-1)) + self.users_start_id)
-        read["aid"] = str(int(random() * (self.number_articles-1)) + self.articles_start_id)
-        
-        region = self.uid_region[read["uid"]]
-        lang = self.aid_lang[read["aid"]]
-        ps = self.p[region + lang]
-
-        if (random() > ps[0]):
-            return self.gen_an_read(i)
-        else:
-            read["read_or_not"] = "1"
-            read["read_time_length"] = str(int(random() * 100))
-            read["read_sequence"] = str(int(random() * 4))
-            read["agree_or_not"] = "1" if random() < ps[1] else "0"
-            read["comment_or_not"] = "1" if random() < ps[2] else "0"
-            read["share_or_not"] = "1" if random() < ps[3] else "0"
-            read["comment_detail"] = "comments to this article: (" + read["uid"] + "," + read["aid"] + ")" 
+        read = self.create_read(i)
         return "(" +  \
-                " " + read["timestamp"] + " , " + \
+                " " + str(read["timestamp"]) + " , " + \
                 " " + read["uid"] + " , " + \
                 " " + read["aid"] + " , " + \
                 "\"" + read["read_or_not"] + "\", " + \
@@ -204,21 +214,21 @@ class GeneratorSQL(Generator):
                 "\"" + read["comment_detail"] + "\")"
 
     def generate(self):
-        with open("user.sql", "w+") as f:
+        with open(os.path.join(self.output_path, "user.sql"), "w+") as f:
             f.write("USE users;\n")
             f.write("INSERT INTO user (timestamp,uid,name,gender,email,phone,dept,grade,language,region,role,prefer_tags,obtained_credits) VALUES\n")
             for i in range (self.users_start_id-1, self.users_start_id + self.number_users-2):
                 f.write("  " + self.gen_an_user(i+1) + ",\n")
             f.write("  " + self.gen_an_user(self.users_start_id + self.number_users-1) + ";\n")
 
-        with open("article.sql", "w+") as f:
+        with open(os.path.join(self.output_path, "article.sql"), "w+") as f:
             f.write("USE articles;\n")
             f.write("INSERT INTO article (timestamp,aid,title,category,abstract,article_tags,authors,language,text,image,video) VALUES\n")
             for i in range(self.articles_start_id - 1, self.articles_start_id + self.number_articles-2):
                 f.write("  " + self.gen_an_article(i+1) + ",\n")
             f.write("  " + self.gen_an_article(self.articles_start_id + self.number_articles-1) + ";\n")
 
-        with open("user_read.sql", "w+") as f:
+        with open(os.path.join(self.output_path, "user_read.sql"), "w+") as f:
             f.write("USE users;\n")
             f.write("INSERT INTO user_read (timestamp,uid,aid,read_or_not,read_time_length,read_sequence,agree_or_not,comment_or_not,share_or_not,comment_detail) VALUES\n")
             for i in range (self.reads_start_id-1, self.reads_start_id+self.number_reads-2):
@@ -276,7 +286,6 @@ def get_args():
     apgreads.add_argument('--reads-hong-kong-zh-ratio-comment', required=False, default=0.2, type=float)
     apgreads.add_argument('--reads-hong-kong-zh-ratio-share', required=False, default=0.1, type=float)
 
-
     args = ap.parse_args()
 
     assert os.path.isdir(args.output_path), 'Output folder not found'
@@ -286,6 +295,6 @@ def get_args():
 
 if __name__ == "__main__":
     args = get_args()
-    t = GeneratorSQL(args)
-    t.generate()
+    gen = GeneratorSQL(args)
+    gen.generate()
     pass
